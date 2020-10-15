eeb6caadc8a2b2702ab31c8abdadcf8976e0534b
xxx <11@11.11>
2020-10-15 20:58:21 +0300

    admin fix

--- kapetelka/urls.py
+++ kapetelka/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from .yasg import urlpatterns as doc_urls
+# from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,4 +15,4 @@ urlpatterns = [
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += doc_urls
+# urlpatterns += doc_urls
